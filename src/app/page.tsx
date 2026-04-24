c5335d9d414b7a62291370ca7d047c8aedf333f7
Hanna Rogalska <hanrogalska@gmail.com>
2026-04-24 19:06:08 +0200

    feat: add hero section with swiper slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client"
 import { Container } from "../components/Container";
 import Header from "../components/layout/Header";
+import { Hero } from "../components/sections/Hero/Hero";
+
+
+
 
 export default function Home() {
   return (
@@ -7,7 +12,11 @@ export default function Home() {
       <Container>
         <Header />
       </Container>
+      <Container size="wide">
+  <Hero/>
+      </Container>
+      
+      
     </>
-
   );
 }
---
+++ src/components/sections/Hero/HeroButton.tsx
@@ -0,0 +1,7 @@
+export const HeroButton = () => {
+  return (
+    <button className="absolute bottom-[160px] left-1/2 z-10 h-14 w-55 -translate-x-1/2 rounded-[10px] border border-[#ffffff] bg-[#ffffff] lg:top-[460px] lg:left-[60px] lg:translate-x-0">
+      Shop the Collection
+    </button>
+  );
+};

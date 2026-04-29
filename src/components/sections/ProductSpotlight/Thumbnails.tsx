76ae57e51585e89accf1563c03be88009c11e2ad
Hanna Rogalska <hanrogalska@gmail.com>
2026-04-29 19:09:40 +0200

    refactor(product-spotlight): migrate images to next/image component

--- src/components/sections/ProductSpotlight/Thumbnails.tsx
+++ src/components/sections/ProductSpotlight/Thumbnails.tsx
@@ -1,25 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import { ThumbnailsProps } from "./types";
 
-
 export const Thumbnails = ({ data, onSelect }: ThumbnailsProps) => {
   return (
-    <div className="flex md:flex-col">
-      {data.length > 0 &&
-        data.map((el, index) => (
-          <button
-            type="button"
-            onClick={() => onSelect(el)}
-            key={`${el}-${index}`}
-            className="md:w-[88px] mr-[16px] md:mb-[24px]"
-          >
-            <img
-              src={el}
-              alt={`thumbnail ${index + 1}`}
-              className="object-cover"
-            />
-          </button>
-        ))}
+    <div className="flex flex-row gap-3 md:flex-col">
+      {data.map((el, index) => (
+        <button
+          key={index}
+          onClick={() => onSelect(el)}
+          className="relative h-[88px] w-[88px] overflow-hidden"
+          type="button"
+        >
+          <Image
+            src={el}
+            alt={`thumbnail ${index + 1}`}
+            fill
+            className="object-cover"
+            sizes="88px"
+          />
+        </button>
+      ))}
     </div>
   );
 };

9d2897efe63da16b2f8d99e5aeb04868e5454032
Hanna Rogalska <hanrogalska@gmail.com>
2026-05-06 14:38:38 +0200

    fix(ui): fix image visibility and layout centering

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { NAV_LINKS } from "./data";
+import { Container } from "../../Container";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,47 +18,49 @@ const Header = () => {
     return () => document.body.classList.remove("overflow-hidden");
   }, [isOpen]);
   return (
-    <header className="relative flex h-[84px] items-center justify-between">
-      <img src="./icon.png" className="hidden h-6 w-6 sm:block" alt="Logo" />
-      <button className="z-50 sm:hidden" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? (
-          <span className="text-2xl">✕</span>
-        ) : (
-          <img
-            src="/images/header/menu-burger.svg"
-            className="h-6 w-6"
-            alt="Burger menu"
-          />
-        )}
-      </button>
-      <nav className="hidden justify-between gap-8 text-base font-normal sm:flex">
-        {NAV_LINKS.map((link) => (
-          <Link
-            key={link.name}
-            href={link.href}
-            className="hover:text-gray-500"
-          >
-            {link.name}
-          </Link>
-        ))}
-      </nav>
-      <Link href="#" className="hover:text-gray-500">
-        <img src="/images/header/bin.png" className="h-6 w-6" alt="Bin" />
-      </Link>
-      {isOpen && (
-        <nav className="absolute top-full left-0 z-40 flex w-full flex-col items-center gap-6 bg-white py-8 shadow-xl md:hidden">
+    <Container>
+      <header className="relative flex h-[84px] items-center justify-between px-[16px] md:px-[60px]">
+        <img src="./icon.png" className="hidden h-6 w-6 sm:block" alt="Logo" />
+        <button className="z-50 sm:hidden" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <span className="text-2xl">✕</span>
+          ) : (
+            <img
+              src="/images/header/menu-burger.svg"
+              className="h-6 w-6"
+              alt="Burger menu"
+            />
+          )}
+        </button>
+        <nav className="hidden justify-between gap-8 text-base font-normal sm:flex">
           {NAV_LINKS.map((link) => (
             <Link
               key={link.name}
               href={link.href}
-              className="text-lg hover:text-gray-500"
+              className="hover:text-gray-500"
             >
               {link.name}
             </Link>
           ))}
         </nav>
-      )}
-    </header>
+        <Link href="#" className="hover:text-gray-500">
+          <img src="/images/header/bin.png" className="h-6 w-6" alt="Bin" />
+        </Link>
+        {isOpen && (
+          <nav className="absolute top-full left-0 z-40 flex w-full flex-col items-center gap-6 bg-white py-8 shadow-xl md:hidden">
+            {NAV_LINKS.map((link) => (
+              <Link
+                key={link.name}
+                href={link.href}
+                className="text-lg hover:text-gray-500"
+              >
+                {link.name}
+              </Link>
+            ))}
+          </nav>
+        )}
+      </header>
+    </Container>
   );
 };
 

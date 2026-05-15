a859e9adc0c33369bcadb113de959591fd5e4f7c
Hanna Rogalska <hanrogalska@gmail.com>
2026-05-15 10:53:04 +0200

    chore(public): convert images to JPG format

--- src/components/sections/FeaturedCollection/data.ts
+++ src/components/sections/FeaturedCollection/data.ts
@@ -3,28 +3,28 @@ import { ShoesType } from "./types";
 export const SHOES_DATA: ShoesType[] = [
   {
     id: "1",
-    src: "/images/cart/shoes-1.png",
+    src: "/images/cart/cart-shoe-1.jpg",
     alt: "Red sneakers",
     title: "Nike Streakfly 2",
     price: 300.0,
   },
   {
     id: "2",
-    src: "/images/cart/shoes-2.png",
+    src: "/images/cart/cart-shoe-2.jpg",
     alt: "White sneakers with a black Swoosh",
     title: "Nike Pegasus Premium",
     price: 350.0,
   },
   {
     id: "3",
-    src: "/images/cart/shoes-3.png",
+    src: "/images/cart/cart-shoe-3.jpg",
     alt: "Black sneakers with a white Swoosh",
     title: "Nike Pegasus 41",
     price: 280.0,
   },
   {
     id: "4",
-    src: "/images/cart/shoes-4.png",
+    src: "/images/cart/cart-shoe-4.jpg",
     alt: "White sneakers",
     title: "Nike Revolution 7",
     price: 440.0,
--- src/components/sections/Hero/data.ts
+++ src/components/sections/Hero/data.ts
@@ -1,7 +1,7 @@
 import { Slide } from "./types";
 
 export const SLIDE_DATA: Slide[] = [
-  { id: 1, alt: "Banner", src: "/images/hero/hero-banner.jpg" },
-  { id: 2, alt: "Air max", src: "/images/hero/air_max.jpg" },
-  { id: 3, alt: "Just do it", src: "/images/hero/shoes.jpg" },
+  { id: "1", alt: "The first banner", src: "/images/hero/hero-1.jpg" },
+  { id: "2", alt: "The second banner", src: "/images/hero/hero-2.jpg" },
+  { id: "3", alt: "The third banner", src: "/images/hero/hero-3.jpg" },
 ];
